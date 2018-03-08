6a1e7a32c4391c0202ad76c6b445688883070d1a
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-03-08 06:55:58 +0100

    Use portable global object for browser compatibility

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const Global = require('the-global-object');
+
 const AsyncFunction = require('./async-function');
 const GeneratorFunction = require('./generator-function');
 const Generator = require('./generator');
 
-const addGlobals = () => Object.assign(global, { AsyncFunction, GeneratorFunction, Generator });
+const addGlobals = () => Object.assign(Global, { AsyncFunction, GeneratorFunction, Generator });
 
 Object.assign(addGlobals, {
   AsyncFunction, GeneratorFunction, Generator, addGlobals,
